53cd3e3697f0433f4cea202f2ee997ee797b307e
Thanos Stamatakis <thanos.stamatakis@gmail.com>
2024-04-25 12:01:04 +0200

    feat: add msw and pollyfills

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,12 @@
 export default {
   testEnvironment: "jsdom",
+  testEnvironmentOptions: {
+    customExportConditions: [""],
+  },
   transform: {
     "^.+\\.tsx?$": "ts-jest",
   },
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts", "<rootDir>/jest.pollyfills.js"],
   // Map aliased paths to the correct directories
   moduleNameMapper: {
     "@trilon/(.*)": "<rootDir>/src/$1",
